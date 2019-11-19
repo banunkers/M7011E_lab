56cd0e5be5657c92792d1936adaee18fc9664855
MTBorg <martinterneborg@protonmail.com>
2019-11-19 14:10:38 +0100

    Add testing suite

---
+++ simulator/tests/schema.test.js
@@ -0,0 +1,95 @@
+const EasyGraphQLTester = require("easygraphql-tester");
+const { expect } = require("chai");
+
+const schema = require("../src/schema.js");
+
+const tester = new EasyGraphQLTester(schema);
+
+describe("GraphQl schema", async () => {
+  describe("Active query", async () => {
+    it("Should return true", async () => {
+      const query = `
+			{
+				active
+			}
+			`;
+      const res = tester.mock({
+        query,
+        fixture: {
+          data: {
+            active: true
+          }
+        }
+      });
+      expect(res.data.active).to.equal("true");
+    });
+  });
+
+  describe("Prosumer", async () => {
+    it("Should have an id", async () => {
+      // TODO: This should use a more direct query and not prosumers
+      const query = `
+			{
+				prosumers{
+					id
+				}
+			}
+			`;
+      const res = tester.mock({
+        query,
+        fixture: {
+          data: {
+            prosumers: [{ id: 4 }]
+          }
+        }
+      });
+      expect(res.data.prosumers[0]).to.have.property("id");
+    });
+
+    it("Should be able to query current consumption", async () => {
+      // TODO: This should use a more direct query and not prosumers
+      const query = `
+			{
+				prosumers{
+					currentConsumption
+				}
+			}
+			`;
+      const res = tester.mock({
+        query,
+        fixture: {
+          data: {
+            prosumers: [{ id: 1, currentConsumption: 5.0 }]
+          }
+        }
+      });
+      expect(res.data.prosumers[0]).to.have.property("currentConsumption");
+      expect(res.data.prosumers[0].currentConsumption).to.be.a("number");
+    });
+  });
+
+  describe("Prosumers query", async () => {
+    it("Should return a list of prosumers", async () => {
+      const query = `
+			{
+				prosumers{
+					id
+				}
+			}
+			`;
+      const res = tester.mock({
+        query,
+        fixture: {
+          data: {
+            prosumers: [{ id: 1 }, { id: 7 }, { id: 3 }]
+          }
+        }
+      });
+      expect(res.data.prosumers).to.be.an("array");
+      expect(res.data.prosumers).to.have.length(3);
+      expect(res.data.prosumers[0].id).to.equal(1);
+      expect(res.data.prosumers[1].id).to.equal(7);
+      expect(res.data.prosumers[2].id).to.equal(3);
+    });
+  });
+});
