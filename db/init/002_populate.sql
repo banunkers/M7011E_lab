ed2f13b873408e49c1f47212345bb7173c757925
banunkers <hugwan-6@student.ltu.se>
2019-11-20 09:58:45 +0100

    Updated db population to only insert values for mean day wind speed in order to enable testing

--- db/init/002_populate.sql
+++ db/init/002_populate.sql
@@ -1,8 +1,8 @@
 \c gle
 
-INSERT INTO prosumers (mean_day_wind_speed, current_wind_speed, current_consumption)
+INSERT INTO prosumers (mean_day_wind_speed)
 VALUES
-	(15, NULL, NULL),
-	(10, NULL, NULL),
-	(5, NULL, NULL),
-	(2, NULL, NULL);
+	(15),
+	(10),
+	(5),
+	(2);
