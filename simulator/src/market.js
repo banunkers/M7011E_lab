45e2967565d84ae69b19841057324bc4202019f6
banunkers <hugwan-6@student.ltu.se>
2019-11-26 10:12:35 +0100

    Added template functions for buying and selling to market, waiting for another branch

---
+++ simulator/src/market.js
@@ -0,0 +1,9 @@
+const { pool } = require("./db.js");
+
+// TODO: wait for Martin branch before implementing
+function sellToMarket(amount) {}
+
+// TODO: wait for Martin branch before implementing
+function buyFromMarket(amount) {}
+
+module.exports = { sellToMarket, buyFromMarket };
