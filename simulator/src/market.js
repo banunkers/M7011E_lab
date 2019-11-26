6b8cd2a546d277aa533fd33fc5a13ab67d5f1b9c
banunkers <hugwan-6@student.ltu.se>
2019-11-26 15:18:08 +0100

    Added queries for buying and selling to/from the market

--- simulator/src/market.js
+++ simulator/src/market.js
@@ -1,9 +1,58 @@
 const { pool } = require("./db.js");
 
-// TODO: wait for Martin branch before implementing
-function sellToMarket(amount) {}
+// NOTE: Needs to change if more power plants
+const sellQuery = `
+WITH old_bat_state AS (
+	SELECT power FROM batteries WHERE id = (SELECT battery_id FROM power_plants WHERE id = 1)	
+)
+UPDATE batteries
+	SET power = (
+		CASE
+		WHEN power + $1 > max_capacity THEN
+			max_capacity
+		ELSE
+			power + $1
+		END
+	)
+	WHERE id = (SELECT battery_id FROM power_plants WHERE id = 1)
+	RETURNING power - (SELECT power FROM old_bat_state) AS sold_amount
+`;
+
+// NOTE: Needs to change if more power plants
+const buyQuery = `
+WITH old_bat_state AS (
+	SELECT power FROM batteries WHERE id = (SELECT battery_id FROM power_plants WHERE id = 1)
+)
+UPDATE batteries
+	SET power = (
+		CASE
+		WHEN power - $1 < 0 THEN
+			0
+		ELSE
+			power - $1
+		END
+	)
+	WHERE id = (SELECT battery_id FROM power_plants WHERE id = 1)
+	RETURNING (SELECT power FROM old_bat_state) - power AS bought_amount
+`;
+
+async function sellToMarket(amount) {
+  let soldAmount = null;
+  await pool
+    .query(sellQuery, [amount])
+    .then(res => (soldAmount = res.rows[0].sold_amount))
+    .catch(err => console.error("Error while selling to market: ", err));
+  return soldAmount;
+}
 
 // TODO: wait for Martin branch before implementing
-function buyFromMarket(amount) {}
+async function buyFromMarket(amount) {
+  let boughtAmount = null;
+  await pool
+    .query(buyQuery, [amount])
+    .then(res => (boughtAmount = res.rows[0].boughtAmount))
+    .catch(err => console.error("Error while buying from the market: ", err));
+  return boughtAmount;
+}
 
 module.exports = { sellToMarket, buyFromMarket };
