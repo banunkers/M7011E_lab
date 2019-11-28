dac8922aabebf890eefe3a7b8919c340a18d7ab8
MTBorg <martinterneborg@protonmail.com>
2019-11-28 15:23:48 +0100

    Fix database error when testing

--- simulator/src/db.js
+++ simulator/src/db.js
@@ -1,14 +1,15 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
-
-pool.on("error", (err, _client) => {
-  console.error(`Unexpected database error: ${err}`);
-});
+// Return object with empty functions if testing since tests run without database
+// but still need the properties in order to be mocked.
+const pool =
+  process.env.NODE_ENV === "test"
+    ? { connect() {}, query() {}, end() {}, on() {} }
+    : new Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+      });
 
 module.exports = { pool };
