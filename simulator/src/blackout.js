afbac8c533a511d876901a1fe1f0069606bf35be
banunkers <hugwan-6@student.ltu.se>
2019-11-29 12:01:36 +0100

    impl function for setting a prosumers blackout status

---
+++ simulator/src/blackout.js
@@ -0,0 +1,21 @@
+const { pool } = require("./db");
+
+const blackoutQuery = `
+UPDATE prosumers SET blackout = $2 WHERE id = $1
+`;
+
+/**
+ * Sets the prosumers house hold blackout status
+ * @param {Number} prosumerId the prosumer id
+ * @param {Boolean} status true or false dependant on blackout or not
+ */
+async function setBlackout(prosumerId, status) {
+  try {
+    await pool.query(blackoutQuery, [prosumerId, status]);
+  } catch (e) {
+    console.error(e);
+  }
+  return status;
+}
+
+module.exports = { setBlackout, blackoutQuery };
