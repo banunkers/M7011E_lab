d67c7bdf112bb0a721f5734195add1650d3caec2
MTBorg <martinterneborg@protonmail.com>
2019-12-10 16:46:52 +0100

    Add login form

--- app/server/server.js
+++ app/server/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../client/views"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("pages/index");
@@ -20,6 +24,10 @@ app.get("/profile", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("pages/register");
+});
+
+app.post("/action/login", req => {
+  console.log(`User login request for ${req.body.email}`);
 });
 
 app.listen(process.env.SERVER_PORT, () => {
