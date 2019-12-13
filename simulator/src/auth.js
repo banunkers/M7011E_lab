c7d1345f58e4456f1f268bff9ab8f936489c4f96
Martin Terneborg <martinterneborg@protonmail.com>
2019-12-13 22:41:46 +0100

    Store user id instead of email in authentication token

--- simulator/src/auth.js
+++ simulator/src/auth.js
@@ -37,11 +37,11 @@ function userIsAdmin(email) {
   return false;
 }
 
-async function checkUserCredentials(user, password) {
+async function checkUserCredentials(userId, password) {
   try {
     const res = await pool.query(
-      "SELECT password_hash FROM prosumers WHERE email=$1",
-      [user]
+      "SELECT password_hash FROM prosumers WHERE id=$1",
+      [userId]
     );
     const hash = res.rows[0].password_hash;
     return bcrypt.compareSync(password, hash);
@@ -52,12 +52,21 @@ async function checkUserCredentials(user, password) {
 
 async function logInUser(email, password) {
   if (email && password) {
-    if (await checkUserCredentials(email, password)) {
-      const isAdmin = userIsAdmin(email);
-      const token = jwt.sign({ email: email, admin: isAdmin }, privateKey, {
-        algorithm: JWT_ALGORITHM
-      });
-      return token;
+    try {
+      const res = await pool.query("SELECT id FROM prosumers WHERE email=$1", [
+        email
+      ]);
+      const id = res.rows[0].id;
+      if (await checkUserCredentials(id, password)) {
+        const isAdmin = userIsAdmin(email);
+        const token = jwt.sign({ id: id, admin: isAdmin }, privateKey, {
+          algorithm: JWT_ALGORITHM
+        });
+        return token;
+      }
+    } catch (error) {
+      console.log(`User login failed ${error}`);
+      return null;
     }
   }
 }
@@ -73,15 +82,14 @@ async function registerUser(email, password) {
 
   try {
     let res = await pool.query(
-      "INSERT INTO prosumers (email, password_hash) VALUES($1, $2)",
+      `INSERT INTO prosumers
+			(email, password_hash)
+			VALUES($1, $2) 
+			RETURNING id`,
       [email, hash]
     );
-<<<<<<< Updated upstream
-    const token = jwt.sign({ email: email }, privateKey, {
-=======
     const userId = res.rows[0].id;
     const token = jwt.sign({ userId, isAdmin: false }, privateKey, {
->>>>>>> Stashed changes
       algorithm: JWT_ALGORITHM
     });
     return token;
