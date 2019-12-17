1020e0e2ad6e0b2f62fcfc5f700bd5c570b98cc6
banunkers <hugwan-6@student.ltu.se>
2019-12-17 10:52:18 +0100

    Merged files

--- app/client/js/prosumer/dashboard.js
+++ app/client/js/prosumer/dashboard.js
@@ -1,9 +1,19 @@
+// Gets all of the prosumer fields aswell as the current electricity pricing
+const QUERY = `{
+	prosumer(id: 1) {
+		... prosumerFields
+	}
+	pricing
+}
+${prosumerFields}
+`;
+
 document.addEventListener("DOMContentLoaded", () => {
   pollFunc(updateData, 10000); // update prosumer data of the dashboard every 10s
 });
 
 async function updateData() {
-  const data = await getProsumerData();
+  const data = await getDashboardData();
   const prosumer = data.prosumer;
   const battery = data.prosumer.battery;
   const pricing = data.pricing;
@@ -28,4 +38,24 @@ async function updateData() {
   ).innerHTML = `${batteryPower}/${batteryMaxCapacity}`;
 
   document.getElementById("pricing").innerHTML = pricing.toFixed(2);
+}
+
+/**
+ * Fetches the prosumer data which will be displayed on the dashboard
+ * @returns an object with subfields prosumer and pricing
+ */
+async function getDashboardData() {
+  let prosumerData = null;
+  await fetch(API_ADDRESS, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      query: QUERY
+    })
+  })
+    .then(res => res.json())
+    .then(res => (prosumerData = res.data));
+  return prosumerData;
 }
