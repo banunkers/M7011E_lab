e2c389b79257abe393b972c7cc0ccda117fadae0
banunkers <hugwan-6@student.ltu.se>
2019-12-17 16:21:01 +0100

    Labels now show the current ratio and they update when you move the range slider

--- app/client/js/prosumer/ratios.js
+++ app/client/js/prosumer/ratios.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const ratios = await getRatios(); // ratios in db stored as decimal
+  const ratios = await getRatios();
   const ratioExcessMarket = ratios.ratioExcessMarket * 100;
   const ratioDeficitMarket = ratios.ratioDeficitMarket * 100;
 
@@ -11,16 +11,45 @@ document.addEventListener("DOMContentLoaded", async () => {
     .getElementById("ratioDeficit")
     .setAttribute("value", ratioDeficitMarket);
 
-  // update db when prosumer changes the ratios
-  document.getElementById("ratioExcess").addEventListener("change", function() {
-    console.log("value after change = ", this.value / 100);
-    setRatioExcess(this.value / 100);
+  // display the ratios as text
+  document.getElementById(
+    "displayExcessBatteryRatio"
+  ).innerText = `Battery: ${100 - ratioExcessMarket}%`;
+  document.getElementById(
+    "displayExcessMarketRatio"
+  ).innerText = `Market: ${ratioExcessMarket}%`;
+  document.getElementById(
+    "displayDeficitBatteryRatio"
+  ).innerText = `Battery: ${100 - ratioDeficitMarket}%`;
+  document.getElementById(
+    "displayDeficitMarketRatio"
+  ).innerText = `Market: ${ratioDeficitMarket}%`;
+
+  // update labels with new
+  document.getElementById("ratioExcess").addEventListener("input", function() {
+    document.getElementById(
+      "displayExcessBatteryRatio"
+    ).innerText = `Battery: ${100 - this.value}%`;
+    document.getElementById(
+      "displayExcessMarketRatio"
+    ).innerText = `Market: ${this.value}%`;
+  });
+  document.getElementById("ratioDeficit").addEventListener("input", function() {
+    document.getElementById(
+      "displayDeficitBatteryRatio"
+    ).innerText = `Battery: ${100 - this.value}%`;
+    document.getElementById(
+      "displayDeficitMarketRatio"
+    ).innerText = `Market: ${this.value}%`;
   });
 
+  // update db when prosumer changes the ratios
+  document.getElementById("ratioExcess").addEventListener("change", function() {
+    setRatioExcess(this.value / 100);
+  });
   document
     .getElementById("ratioDeficit")
     .addEventListener("change", function() {
-      console.log("value after change = ", this.value / 100);
       setRatioDeficit(this.value / 100);
     });
 });
