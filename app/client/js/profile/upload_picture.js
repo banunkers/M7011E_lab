5b26c48b7901d1d6b83d736b74944d0ac80206ad
Martin Terneborg <martinterneborg@protonmail.com>
2019-12-25 11:55:44 +0000

    Update profile image automatically on successfull upload

--- app/client/js/profile/upload_picture.js
+++ app/client/js/profile/upload_picture.js
@@ -24,19 +24,15 @@ function uploadImageFormSubmit(event) {
   const reader = new FileReader();
   const authToken = getCookie("authToken", document.cookie);
   reader.onload = async e => {
-    try {
-      const image = e.target.result;
-      fetch(`${API_REST_ADDRESS}/upload_image`, {
-        method: "POST",
-        headers: { "content-type": "application/json", authToken },
-        body: JSON.stringify({ image })
-      })
-        .then(res => res.json())
-        .then(res => console.log(res))
-        .catch(error => alert(`Failed to upload image: ${error}`));
-    } catch (error) {
-      alert(`Failed to upload image: ${error}`);
-    }
+    const image = e.target.result;
+    fetch(`${API_REST_ADDRESS}/upload_image`, {
+      method: "POST",
+      headers: { "content-type": "application/json", authToken },
+      body: JSON.stringify({ image })
+    })
+      .then(res => res.json())
+      .then(res => (document.getElementById("image").src = image))
+      .catch(error => alert(`Failed to upload image: ${error}`));
   };
   reader.readAsDataURL(file);
 }
