de4cfbba2bd24e8ca4da470e50003c435ec2017a
banunkers <hugwan-6@student.ltu.se>
2020-01-10 10:07:53 +0100

    Prices are displayed and can be changed

---
+++ app/client/js/dashboard/manager/pricing.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // update price to catch price changes from other managers
+  pollFunc(getPricing, INTERVAL_TIMER);
+});
+
+async function getPricing() {
+  const PRICING_QUERY = `{
+		simPricing,
+		pricing
+	}`;
+  const authToken = getCookie("authToken", document.cookie);
+  let prices = null;
+  try {
+    await fetch(API_ADDRESS, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authToken
+      },
+      body: JSON.stringify({
+        query: PRICING_QUERY
+      })
+    })
+      .then(res => res.json())
+      .then(res => (prices = res.data));
+  } catch (err) {
+    console.error(`Failed to get pricing: ${err}`);
+  }
+  displayPrices(prices.pricing, prices.simPricing);
+}
+
+function displayPrices(price, simPrice) {
+  document.getElementById("currPrice").innerHTML = price;
+  document.getElementById("suggestedPrice").innerHTML = simPrice;
+  document.getElementById("diffPrice").innerHTML =
+    price - simPrice > 0
+      ? `+${(price - simPrice).toFixed(2)}`
+      : (price - simPrice).toFixed(2);
+}
+
+async function submitPricingForm(event, form) {
+  event.preventDefault();
+  const price = form[0].value;
+  const SET_PRICE_QUERY = `mutation {
+		setPricing(price: ${price})
+	}`;
+  const authToken = getCookie("authToken", document.cookie);
+  try {
+    await fetch(API_ADDRESS, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authToken
+      },
+      body: JSON.stringify({
+        query: SET_PRICE_QUERY
+      })
+    });
+  } catch (err) {
+    console.error(`Failed to get pricing: ${err}`);
+    alert(`Failed to update price: ${err.message}`);
+    return;
+  }
+
+  displayPrices(price, document.getElementById("suggestedPrice").innerHTML);
+  form.reset();
+  alert("Price updated");
+}
