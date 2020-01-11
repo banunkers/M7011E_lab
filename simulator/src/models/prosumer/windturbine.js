950777d5398037050f4b0b431492c890aec9eefc
banunkers <hugwan-6@student.ltu.se>
2020-01-11 14:24:40 +0100

    More pruning

--- simulator/src/models/prosumer/windturbine.js
+++ simulator/src/models/prosumer/windturbine.js
@@ -1,10 +1,10 @@
 // Turbine configs
 const cp = 0.45; // the power coefficent
 const airDensity = 1.25; // [kg/m^2]
-const rotorDiameter = 6; // rotor diameter [m]
+const rotorDiameter = 4; // rotor diameter [m]
 const cutInSpeed = 2; // at what wind speed the turbine starts producing power [m/s]
 const cutOutSpeed = 15; // at what wind speed the turbine shuts down to minimize damages [m/s]
-const ratedOutputPower = 6; // the turbines maximum power output [kW]
+const ratedOutputPower = 5.5; // the turbines maximum power output [kW]
 
 function turbineOutput(windSpeed) {
   if (windSpeed > cutInSpeed && windSpeed < cutOutSpeed) {
