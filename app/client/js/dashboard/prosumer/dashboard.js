d749b722e29de717e21cbe7d365c1f0a7e54003f
Martin Terneborg <martinterneborg@protonmail.com>
2020-01-17 11:19:44 +0100

    Restructure code

--- app/client/js/dashboard/prosumer/dashboard.js
+++ app/client/js/dashboard/prosumer/dashboard.js
@@ -1,36 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-  pollFunc(updateData, 10000); // update prosumer data of the dashboard every 10s
+  pollFunc(updateData, 3000);
 });
 
 async function updateData() {
-  const data = await getData();
-  const prosumer = data.me;
-  const battery = data.me.battery;
-  const pricing = data.pricing;
+  try {
+    const authToken = getCookie("authToken", document.cookie);
+    const response = await fetch(API_ADDRESS, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authToken
+      },
+      body: JSON.stringify({
+        query: `
+				{
+					me{
+						... on prosumer{
+							currentWindSpeed
+							currentProduction
+							currentConsumption
+							blocked
+							battery{
+								power
+								maxCapacity
+							}
+						}
+					}
+					pricing
+				}
+				`
+      })
+    });
+    const json = await response.json();
+    const prosumer = json.data.me;
+    const { battery } = json.data.me;
+    const { pricing } = json.data;
 
-  document.getElementById(
-    "windSpeed"
-  ).innerHTML = prosumer.currentWindSpeed.toFixed(2);
-  document.getElementById(
-    "production"
-  ).innerHTML = prosumer.currentProduction.toFixed(2);
-  document.getElementById(
-    "consumption"
-  ).innerHTML = prosumer.currentConsumption.toFixed(2);
+    document.getElementById(
+      "windSpeed"
+    ).innerHTML = prosumer.currentWindSpeed.toFixed(2);
+    document.getElementById(
+      "production"
+    ).innerHTML = prosumer.currentProduction.toFixed(2);
+    document.getElementById(
+      "consumption"
+    ).innerHTML = prosumer.currentConsumption.toFixed(2);
 
   document.getElementById("block-status").innerHTML = prosumer.blocked
     ? "<i data-feather='x-octagon' style=color:red></i> You are blocked from selling"
     : "";
   feather.replace();
 
-  const netProd = prosumer.currentProduction - prosumer.currentConsumption;
-  document.getElementById("netProduction").innerHTML = netProd.toFixed(2);
+    const netProd = prosumer.currentProduction - prosumer.currentConsumption;
+    document.getElementById("netProduction").innerHTML = netProd.toFixed(2);
 
-  const batteryPower = battery.power.toFixed(2);
-  const batteryMaxCapacity = battery.maxCapacity.toFixed(2);
-  document.getElementById(
-    "battery"
-  ).innerHTML = `${batteryPower}/${batteryMaxCapacity}`;
+    const batteryPower = battery.power.toFixed(2);
+    const batteryMaxCapacity = battery.maxCapacity.toFixed(2);
+    document.getElementById(
+      "battery"
+    ).innerHTML = `${batteryPower}/${batteryMaxCapacity}`;
 
-  document.getElementById("pricing").innerHTML = pricing.toFixed(2);
+    document.getElementById("pricing").innerHTML = pricing.toFixed(2);
+  } catch (error) {
+    console.error(`Failed to update dashboard update: ${error}`);
+  }
 }
