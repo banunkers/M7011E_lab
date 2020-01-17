01cef8ff4a36e1a21c6c04333c1ffbbcd2cdf074
Martin Terneborg <martinterneborg@protonmail.com>
2020-01-17 10:20:26 +0100

    Add functionality for using one query to get all manager dashboard data

---
+++ app/client/js/dashboard/manager/pollData.js
@@ -0,0 +1,43 @@
+const TEST_INTERVAL = 3000;
+const POLL_MESSAGE_NAME = "POLL_UPDATE";
+
+document.addEventListener("DOMContentLoaded", () => {
+  pollFunc(getDashboardData, TEST_INTERVAL);
+});
+
+async function getDashboardData() {
+  const query = `
+	{
+		simPricing,
+		pricing
+	}
+	`;
+  const authToken = getCookie("authToken", document.cookie);
+  try {
+    const response = await fetch(API_ADDRESS, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authToken
+      },
+      body: JSON.stringify({
+        query
+      })
+    });
+    const json = await response.json();
+    if (json.errors != null) {
+      throw new Error(json.errors[0].message);
+    }
+
+    const event = new CustomEvent(POLL_MESSAGE_NAME, {
+      detail: { data: json.data }
+    });
+    document.dispatchEvent(event);
+  } catch (err) {
+    console.error(`Failed to get poll dashboard data: ${err}`);
+  }
+}
+
+function registerPollCallback(callback) {
+  document.addEventListener(POLL_MESSAGE_NAME, callback);
+}
