fcf5696fe0913a030a8c9713b91158ff53fdaecf
Martin Terneborg <martinterneborg@protonmail.com>
2020-01-17 09:30:18 +0100

    Fix error message being displayed on manager dashboard when a new user registers

--- app/client/js/dashboard/manager/prosumerOverview/prosumerOverview.js
+++ app/client/js/dashboard/manager/prosumerOverview/prosumerOverview.js
@@ -1,4 +1,4 @@
-const POLL_INTERVAL = 3000;
+// const POLL_INTERVAL = 3000;
 document.addEventListener("DOMContentLoaded", () => {
   pollFunc(updateData, POLL_INTERVAL);
 });
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 async function updateData() {
   const authToken = getCookie("authToken", document.cookie);
   try {
-    let response = await fetch(API_ADDRESS, {
+    const response = await fetch(API_ADDRESS, {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -21,6 +21,7 @@ async function updateData() {
 						blackout
 						account {
 							online
+							email
 						}
 					}
 				}
@@ -31,15 +32,50 @@ async function updateData() {
     let numBlackout = 0;
     json.data.prosumers.forEach(prosumer => {
       if (prosumer.blackout) numBlackout++;
-      document.getElementById(
-        `blocked-status-${prosumer.id}`
-      ).innerText = prosumer.blocked ? "\u2714" : "";
-      document.getElementById(
-        `blackout-status-${prosumer.id}`
-      ).innerText = prosumer.blackout ? "\u2714" : "";
-      document.getElementById(
-        `online-status-${prosumer.id}`
-      ).innerText = prosumer.account.online ? "\u2714" : "";
+      const rowId = `prosumer-${prosumer.id}`;
+      const onlineCellId = `online-status-${prosumer.id}`;
+      const blockStatusCellId = `blocked-status-${prosumer.id}`;
+      const blackoutCellId = `blackout-status-${prosumer.id}`;
+      if (!document.getElementById(rowId)) {
+        const table = document.getElementById("prosumer-overview-body");
+
+        const row = table.insertRow(0);
+        row.id = rowId;
+
+        // Insert cells
+        const idCell = row.insertCell(0);
+        const onlineCell = row.insertCell(1);
+        const blockBtnCell = row.insertCell(2);
+        const blockStatusCell = row.insertCell(3);
+        const blackoutCell = row.insertCell(4);
+
+        idCell.innerHTML = `
+				<a href="/prosumer-summary/${prosumer.id}">${prosumer.id} - ${prosumer.account.email}</a>
+				`;
+        onlineCell.innerHTML = `
+				<span id="${onlineCellId}"> ${prosumer.account.online ? "\u2714" : ""}</span>
+				`;
+        blockBtnCell.innerHTML = `
+				<button class="btn btn-danger" onclick="blockProsumer(${prosumer.id})">Block</button>
+				`;
+        blockStatusCell.innerHTML = `
+					<span id="${blockStatusCellId}">${prosumer.blocked ? "\u2714" : ""}</span>
+				`;
+        blackoutCell.innerHTML = `
+				<span id=${blackoutCellId}> ${prosumer.blackout ? "\u2714" : ""} </span>
+				`;
+      } else {
+        document.getElementById(blockStatusCellId).innerText = prosumer.blocked
+          ? "\u2714"
+          : "";
+        document.getElementById(blackoutCellId).innerText = prosumer.blackout
+          ? "\u2714"
+          : "";
+        document.getElementById(onlineCellId).innerText = prosumer.account
+          .online
+          ? "\u2714"
+          : "";
+      }
     });
     document.getElementById("prosumer-status").innerText =
       numBlackout > 0
