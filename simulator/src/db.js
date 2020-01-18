ab0a9811ffaf7ef4c3d1cbc1384bdad9a5eaf267
banunkers <hugwan-6@student.ltu.se>
2020-01-18 13:44:02 +0100

    Added default coninfo to db

--- simulator/src/db.js
+++ simulator/src/db.js
@@ -1,15 +1,20 @@
 const { Pool } = require("pg");
 
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USER = process.env.DB_USER || "gle";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_DATABASE = process.env.DB_DATABASE || "gle";
+
 // Return object with empty functions if testing since tests run without database
 // but still need the properties in order to be mocked.
 const pool =
   process.env.NODE_ENV === "test"
     ? { connect() {}, query() {}, end() {}, on() {} }
     : new Pool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE
       });
 
 module.exports = { pool };
